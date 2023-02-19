ebb1b6550e7a4f6a90e253286bb96d29c1702d1c
Verah Nyagaka <verahnyagaka101@gmail.com>
2023-02-19 21:55:40 +0300

    updated code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { DisplayComponent } from './display/display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AddTaskComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule
